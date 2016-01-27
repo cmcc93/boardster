73cff5b5cfaadcccf38bc5c7d9998d4f2147bf4b
Casey McCourt <cas.mcco@gmail.com>
2016-01-27 15:05:00 -0800

    beginning of rearrangement

---
+++ Boardster/LocationsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LocationsTabViewController.swift
+//  Boardster
+//
+//  Created by Casey McCourt on 1/24/16.
+//  Copyright © 2016 McCourt Industries. All rights reserved.
+//
+
+import UIKit
+
+class LocationsTabViewController: UIViewController {
+    
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if self.revealViewController() != nil {
+            menuButton.target = self.revealViewController()
+            menuButton.action = Selector("revealToggle:")
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}

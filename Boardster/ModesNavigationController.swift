c1721b6fe5579fef6ee512587ee7bc43f2fe9d35
Casey McCourt <cas.mcco@gmail.com>
2016-01-31 19:06:09 -0800

    playing with background photos and translucent navbar

---
+++ Boardster/ModesNavigationController.swift
@@ -0,0 +1,41 @@
+//
+//  ModesNavigationController.swift
+//  Boardster
+//
+//  Created by Casey McCourt on 1/31/16.
+//  Copyright © 2016 McCourt Industries. All rights reserved.
+//
+
+import UIKit
+
+class ModesNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
+        
+        self.navigationBar.setBackgroundImage(UIImage(), forBarMetrics: UIBarMetrics.Default)
+        self.navigationBar.shadowImage = UIImage()
+        self.navigationBar.translucent = true
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

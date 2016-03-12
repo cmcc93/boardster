5582f4dc4de5695b4413e656eb9d87590a3ee07e
Casey McCourt <cas.mcco@gmail.com>
2016-03-11 18:15:05 -0800

    playing around with changing class of profile view controller

---
+++ Boardster/ProfileTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileTableViewController.swift
+//  Boardster
+//
+//  Created by Casey McCourt on 3/11/16.
+//  Copyright © 2016 McCourt Industries. All rights reserved.
+//
+
+import UIKit
+
+class ProfileTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+}

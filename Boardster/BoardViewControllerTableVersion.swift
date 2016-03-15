aabee6a7f04dbae49216422a5df3e6aedfd3941c
Casey McCourt <cas.mcco@gmail.com>
2016-03-15 13:35:39 -0700

    view controller made table version

--- Boardster/BoardViewControllerTableVersion.swift
+++ Boardster/BoardViewControllerTableVersion.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class BoardViewControllerTableVersion: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class BoardViewControllerTableVersion: UITableViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     // MARK: Properties
     

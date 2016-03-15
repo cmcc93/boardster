e77fbb24c9232fe3a617a2a60988577312424dd4
Casey McCourt <cas.mcco@gmail.com>
2016-03-15 13:25:30 -0700

    board view controller table version admin added

---
+++ Boardster/BoardViewControllerTableVersion.swift
@@ -0,0 +1,135 @@
+//
+//  BoardViewControllerTableVersion.swift
+//  Boardster
+//
+//  Created by Casey McCourt on 3/15/16.
+//  Copyright © 2016 McCourt Industries. All rights reserved.
+//
+
+import UIKit
+
+class BoardViewControllerTableVersion: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    // MARK: Properties
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    @IBOutlet weak var lengthTextField: UITextField!
+    
+    var board: Board?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Handle the text field’s user input through delegate callbacks.
+        if nameTextField != nil {
+            nameTextField.delegate = self
+        }
+        
+        lengthTextField.delegate = self
+        
+        // Set up views if editing an existing Board.
+        if let board = board {
+            navigationItem.title = board.name
+            if nameTextField != nil {
+                nameTextField.text = board.name
+            }
+            photoImageView.image = board.photo
+            ratingControl.rating = board.rating
+            lengthTextField.text = board.length
+        }
+        
+        // Enable the Save button only if the text field has a valid Board name.
+        checkValidBoardName()
+
+    }
+
+    // MARK: UITextFieldDelegate
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        checkValidBoardName()
+        navigationItem.title = textField.text
+    }
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        // Disable the Save button while editing.
+        saveButton.enabled = false
+    }
+    
+    func checkValidBoardName() {
+        // Disable the Save button if the text field is empty.
+        let text = nameTextField.text ?? ""
+        saveButton.enabled = !text.isEmpty
+    }
+    
+    // MARK: UIImagePickerControllerDelegate
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        // Dismiss the picker if the user canceled.
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        // The info dictionary contains multiple representations of the image, and this uses the original.
+        let selectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        // Set photoImageView to display the selected image.
+        photoImageView.image = selectedImage
+        
+        // Dismiss the picker.
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    // MARK: Navigation
+    
+    @IBAction func cancel(sender: UIBarButtonItem) {
+        // Depending on style of presentation (modal or push presentation), this view controller needs to be dismissed in two different ways.
+        let isPresentingInAddBoardMode = presentingViewController is UINavigationController
+        
+        if isPresentingInAddBoardMode {
+            dismissViewControllerAnimated(true, completion: nil)
+        } else {
+            navigationController!.popViewControllerAnimated(true)
+        }
+    }
+    
+    // This method lets you configure a view controller before it's presented.
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if saveButton === sender {
+            let name = nameTextField.text ?? ""
+            let photo = photoImageView.image
+            let rating = ratingControl.rating
+            let length = lengthTextField.text
+            
+            // Set the board to be passed to BoardListTableViewController after the unwind segue.
+            board = Board(name: name, photo: photo, rating: rating, length: length!)
+        }
+    }
+    
+    // MARK: Actions
+    
+    @IBAction func selectImageFromPhotoLibrary(sender: UITapGestureRecognizer) {
+        // Hide the keyboard.
+        nameTextField.resignFirstResponder()
+        
+        // UIImagePickerController is a view controller that lets a user pick media from their photo library.
+        let imagePickerController = UIImagePickerController()
+        
+        // Only allow photos to be picked, not taken.
+        imagePickerController.sourceType = .PhotoLibrary
+        
+        // Make sure ViewController is notified when the user picks an image.
+        imagePickerController.delegate = self
+        
+        presentViewController(imagePickerController, animated: true, completion: nil)
+    }
+    
+}

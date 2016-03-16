f8b304341fa37d1eea0b6507d451b3c9ff6df677
Casey McCourt <cas.mcco@gmail.com>
2016-03-15 19:38:49 -0700

    more of the nonadmin hookup working

--- Boardster/BoardViewControllerTableVersionNonAdmin.swift
+++ Boardster/BoardViewControllerTableVersionNonAdmin.swift
@@ -9,11 +9,27 @@
 import UIKit
 
 class BoardViewControllerTableVersionNonAdmin: BoardViewControllerTableVersion {
-
+    
+    @IBOutlet weak var nameLabel: UILabel!
+//    @IBOutlet weak var lengthTextField: UITextField!
+    
+    @IBOutlet weak var storedPhotoImageView: UIImageView!
+    @IBOutlet weak var storedRatingControl: RatingControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
+        
+        if let board = board {
+            nameLabel.text = board.name
+            storedPhotoImageView.image = board.photo
+            storedRatingControl.rating = board.rating
+            lengthTextField.text = board.length
+        }
+        
+    }
+    
+    override func checkValidBoardName() {
+        
     }
 
 }

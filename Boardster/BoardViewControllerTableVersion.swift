b701761f69400bfa41196511373d29daf4d7aba6
Casey McCourt <cas.mcco@gmail.com>
2016-03-21 14:50:58 -0700

    more model additions

--- Boardster/BoardViewControllerTableVersion.swift
+++ Boardster/BoardViewControllerTableVersion.swift
@@ -116,12 +116,13 @@ class BoardViewControllerTableVersion: UITableViewController, UITextFieldDelegat
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if saveButton === sender {
             let name = nameTextField.text ?? ""
+            let model = modelTextField.text ?? ""
             let photo = photoImageView.image
             let rating = ratingControl.rating
             let length = lengthTextField.text
             
             // Set the board to be passed to BoardListTableViewController after the unwind segue.
-            board = Board(name: name, photo: photo, rating: rating, length: length!)
+            board = Board(name: name, model: model, photo: photo, rating: rating, length: length!)
         }
     }
     

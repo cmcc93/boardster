20f368653416fe32db44c9bb10d5b771b499140b
Casey McCourt <cas.mcco@gmail.com>
2016-03-25 11:51:28 -0700

    not sure

--- Boardster/SnowboardLocationViewController.swift
+++ Boardster/SnowboardLocationViewController.swift
@@ -19,9 +19,6 @@ class SnowboardLocationViewController: UIViewController {
     }
     
     
-//    @IBAction func surfHomeToTable(sender: UIButton) {
-//        performSegueWithIdentifier("surfboardsHomeToBoardsTable", sender: nil)
-//    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,20 +26,13 @@ class SnowboardLocationViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        
+        var destination = segue.destinationViewController as? UITableViewController
+        
+        if let navCon = destination as? ModesNavigationController {
+            destination = navCon.topViewController
+        }
     }
-    */
 
 }

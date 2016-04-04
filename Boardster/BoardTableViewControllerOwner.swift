3b3d65b16037e2439d02109fba54ede72ac17149
Casey McCourt <cas.mcco@gmail.com>
2016-04-04 13:11:32 -0700

    added new view controller class for owners

---
+++ Boardster/BoardTableViewControllerOwner.swift
@@ -0,0 +1,18 @@
+//
+//  BoardTableViewControllerOwner.swift
+//  Boardster
+//
+//  Created by Casey McCourt on 4/4/16.
+//  Copyright © 2016 McCourt Industries. All rights reserved.
+//
+
+import UIKit
+
+class BoardTableViewControllerOwner: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
